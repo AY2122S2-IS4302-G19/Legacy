87efe652579c3e1d68235953766f56e51ad32e8b
Darren <darrenljw96@gmail.com>
2022-03-03 10:34:47 +0800

    Added part of initial code in legacy.sol

--- contracts/legacy/Legacy.sol
+++ contracts/legacy/Legacy.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
+// pragma experimental ABIEncoderV2; //Need to be included for Will struct to be used as parameter inside functions
 
 contract Legacy {
     
+    struct Will {
+        uint id;
+        bool initalized; //false by default, helps to check if will exist
+    }
+
+    mapping(address => Will) users;
+    address _owner;
+
+
+    modifier validUser(address user) {
+        require(users[user].initalized);
+        _;
+    }
+
+    function addUser() public {
+        Will memory will = Will(0,true);
+        users[msg.sender] = will;
+    }
 }

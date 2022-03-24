8eb5d512b08e4062f47f028cebb611b2eb894661
hongpeigoh <57928283+hongpeigoh@users.noreply.github.com>
2022-03-24 16:35:07 +0800

    readability

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Legacy = artifacts.require("LegacyToken");
+const LegacyToken = artifacts.require("LegacyToken");
 
 module.exports = (deployer, network, accounts) => {
-  deployer.deploy(Legacy);
+  deployer.deploy(LegacyToken);
 };
